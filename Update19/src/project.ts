4e44e54cb4c6790988bf69aaeee6846e67fc30bf
Zachary Ross <zacharyross1234@gmail.com>
2024-07-09 16:23:57 -0400

    Player Hosted Games

---
+++ Update19/src/project.ts
@@ -0,0 +1,9 @@
+import {makeProject} from '@motion-canvas/core';
+
+import PlayerHostedGames from './scenes/PlayerHostedGames?scene';
+
+
+export default makeProject({
+  scenes: [PlayerHostedGames],
+  //audio: audioBots3,
+});
